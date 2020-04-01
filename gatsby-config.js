4c3f9a56d108e3e4a3d46eab7c915f0625a1d32c
Akash Bhave <akashbbhave@gmail.com>
2020-04-01 13:59:11 -0400

    Initialize Gatsby project

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteMetadata: {
+    title: `Fighting COVID-19`,
+    description: `Official website for the Fighting COVID-19 movement.`,
+    author: `Fighting COVID-19`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Fighting COVID-19`,
+        short_name: `Fighting COVID`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+};
