f37f55b09d2806c5510ca5a72b840aa766050bd0
Akash Bhave <akashbbhave@gmail.com>
2020-04-02 21:46:10 -0400

    Create header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,46 @@
+import React, { useState } from "react";
 import { Link } from "gatsby";
-import PropTypes from "prop-types";
-import React from "react";
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div>Header</div>
-  </header>
-);
+import "hamburgers/dist/hamburgers.min.css";
+import styles from "./header.module.css";
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-};
+const Header = () => {
+  const [isActive, setActive] = useState(false);
 
-Header.defaultProps = {
-  siteTitle: ``,
+  return (
+    <header id="header" className={styles.header}>
+      <button
+        onClick={() => setActive(!isActive)}
+        className={`hamburger hamburger--squeeze ${
+          isActive ? "is-active" : ""
+        } ${styles.hamburgerContainer}`}
+        type="button"
+      >
+        <span className="hamburger-box">
+          <span className={`hamburger-inner ${styles.hamburgerInner}`}></span>
+        </span>
+      </button>
+      <nav className={`${styles.nav} ${isActive ? "block" : "hidden"}`}>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/donate">Donate</Link>
+          </li>
+          <li className="font-bold">
+            <Link to="/">Project YCRO</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
