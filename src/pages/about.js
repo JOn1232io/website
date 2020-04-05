1d2a219d6c4304b04d0ec85368bd130606af1fa3
Akash Bhave <akashbbhave@gmail.com>
2020-04-04 20:25:21 -0400

    Refactor about page to remove extra functions

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+
 import data from "../data/about.yml";
 
 const AboutPage = () => (
@@ -32,7 +33,32 @@ const AboutPage = () => (
           Our Team
         </h1>
         <section className="p-4 pb-0 bg-white color-dark block">
-          {data.content.map(getContent)}
+          {data.content.map(team => (
+            <div className="pb-4">
+              <h2 className="font-normal mb-2 first:bg-highlight text-2xl">
+                {team.category}
+              </h2>
+              <div
+                className="members"
+                style={{
+                  display: `grid`,
+                  gridTemplateColumns: `repeat(auto-fit, minmax(20rem, 1fr))`,
+                  gap: `1rem`,
+                }}
+              >
+                {team.members.map(member => (
+                  <span
+                    className="member bg-highlight text-white p-4 font-bold uppercase tracking-wide"
+                    style={{
+                      boxShadow: `0 0.125rem 0.125rem -0.125rem black`,
+                    }}
+                  >
+                    {member.name}
+                  </span>
+                ))}
+              </div>
+            </div>
+          ))}
         </section>
       </section>
     </section>
@@ -40,36 +66,3 @@ const AboutPage = () => (
 );
 
 export default AboutPage;
-
-function getContent(object) {
-  return (
-    <div className="pb-4">
-      <h2 className="font-normal mb-2 first:bg-highlight text-2xl">
-        {object.category}
-      </h2>
-      <div
-        className="members"
-        style={{
-          display: `grid`,
-          gridTemplateColumns: `repeat(auto-fit, minmax(20rem, 1fr))`,
-          gap: `1rem`,
-        }}
-      >
-        {object.members.map(getName)}
-      </div>
-    </div>
-  );
-}
-
-function getName(object) {
-  return (
-    <span
-      className="member bg-highlight text-white p-4 font-bold uppercase tracking-wide"
-      style={{
-        boxShadow: `0 0.125rem 0.125rem -0.125rem black`,
-      }}
-    >
-      {object.name}
-    </span>
-  );
-}
