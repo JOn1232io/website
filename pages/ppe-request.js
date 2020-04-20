34ee5d29c5eacd9088b8e5f68bff27f7bb821ba6
Jason Klein <kleinjason234@gmail.com>
2020-04-20 11:31:32 -0400

    Update request and donate pages

--- pages/ppe-request.js
+++ pages/ppe-request.js
@@ -1,23 +1,23 @@
-import SEO from "../components/seo";
+import SEO from "../components/seo"
 
 const PPERequestPage = ({ data }) => (
-  <>
-    <SEO title="Request PPE" />
-    <main className="container mx-auto">
-      <section className="p-8" style={{ height: "calc(100vh - 100px)" }}>
-        <iframe
-          style={{ height: "100%" }}
-          src="https://docs.google.com/forms/d/e/1FAIpQLSfDdXO8zSeozASqR8GUQDwsmXvUPbx1BGUveNh1Yu8fm7omuQ/viewform?usp=sf_link&embedded=true"
-          width="100%"
-          frameBorder="0"
-          marginHeight="0"
-          marginWidth="0"
-        >
-          Loading…
-        </iframe>
-      </section>
-    </main>
-  </>
-);
+	<>
+		<SEO title="Request PPE" />
+		<main>
+			<section style={{ width: "100vw", marginTop: "3rem", height: "calc(100vh - 6rem)" }}>
+				<iframe
+					style={{ height: "100%" }}
+					src="https://docs.google.com/forms/d/e/1FAIpQLSfDdXO8zSeozASqR8GUQDwsmXvUPbx1BGUveNh1Yu8fm7omuQ/viewform?usp=sf_link&embedded=true"
+					width="100%"
+					height="100%"
+					frameBorder="0"
+					marginHeight="0"
+					marginWidth="0">
+					Loading…
+				</iframe>
+			</section>
+		</main>
+	</>
+)
 
-export default PPERequestPage;
+export default PPERequestPage
