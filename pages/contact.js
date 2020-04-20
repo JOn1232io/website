226e17e4e4fe5010956f4da2bbef5c0458c55642
Jason Klein <kleinjason234@gmail.com>
2020-04-20 13:23:22 -0400

    Update contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,127 +1,129 @@
-import Head from "next/head";
-import yaml from "js-yaml";
-import fs from "fs";
-import path from "path";
-import axios from "axios";
-import { useState } from "react";
+import Head from "next/head"
+import yaml from "js-yaml"
+import fs from "fs"
+import path from "path"
+import axios from "axios"
+import { useState } from "react"
 
-import Markdown from "react-markdown";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import SEO from "../components/seo";
+import Markdown from "react-markdown"
+import { Formik, Form, Field, ErrorMessage } from "formik"
+import SEO from "../components/seo"
 
 const FormField = ({ title, type }) => (
-  <div className="mb-4">
-    <label className="text-lg block mb-2">{title}</label>
-    <Field
-      type={type}
-      as={type === "textarea" ? "textarea" : "input"}
-      rows="6"
-      name={title.toLowerCase()}
-      className="text-lg w-full border-2 border-gray-500 focus:border-accent rounded p-4 mb-2"
-    />
-    <ErrorMessage
-      name={title.toLowerCase()}
-      component="div"
-      className="text-accent mb-6"
-    />
-  </div>
-);
+	<div className="mb-4">
+		<label className="text-lg block mb-2">{title}</label>
+		<Field
+			type={type}
+			as={type === "textarea" ? "textarea" : "input"}
+			rows="6"
+			name={title.toLowerCase()}
+			className="text-lg w-full border-2 border-gray-500 focus:border-accent rounded p-4 mb-2"
+		/>
+		<ErrorMessage name={title.toLowerCase()} component="div" className="text-accent mb-6" />
+	</div>
+)
 
 const ContactPage = ({ data }) => {
-  const [formResult, setFormResult] = useState(null);
+	const [formResult, setFormResult] = useState(null)
 
-  return (
-    <>
-      <SEO title="Contact" />
-      <main className="container mx-auto">
-        <section className="p-8">
-          <h2 className="font-serif font-bold md:text-5xl text-4xl pb-4">
-            {data.title}
-          </h2>
-          <Markdown className="text-xl markdown-body mb-4">
-            {data.subtitle}
-          </Markdown>
-          {formResult === null || (
-            <p
-              className={`text-xl ${
-                formResult.success ? "text-green-600" : "text-red-700"
-              }`}
-            >
-              {formResult.message}
-            </p>
-          )}
-        </section>
-        <section className="p-8">
-          <Formik
-            initialValues={{ name: "", email: "", message: "" }}
-            validate={(values) => {
-              const errors = {};
+	return (
+		<>
+			<SEO title="Contact" />
+			<main className="container mx-auto mt-16">
+				<article style={{ margin: "calc(3rem + 5rem) 0 5rem 0" }}>
+					<h1
+						style={{
+							padding: "0.5rem 2rem",
+							color: "#304352",
+							textAlign: "center",
+							textTransform: "uppercase",
+							fontSize: "200%",
+							fontWeight: "bold",
+						}}>
+						{data.title}
+					</h1>
+					<section>
+						<p style={{ margin: "auto 10% 5rem 10%", lineHeight: "250%", textAlign: "center" }}>
+							<Markdown
+								style={{ margin: "auto 10% 5rem 10%", lineHeight: "250%", textAlign: "center" }}>
+								{data.subtitle}
+							</Markdown>
+							{formResult === null || (
+								<p className={`text-xl ${formResult.success ? "text-green-600" : "text-red-700"}`}>
+									{formResult.message}
+								</p>
+							)}
+						</p>
+					</section>
+				</article>
 
-              if (!values.name) {
-                errors.name = "A name is required";
-              }
+				<section className="pb-8 px-8">
+					<Formik
+						initialValues={{ name: "", email: "", message: "" }}
+						validate={values => {
+							const errors = {}
 
-              if (!values.email) {
-                errors.email = "An email is required";
-              } else if (
-                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-              ) {
-                errors.email = "Invalid email address";
-              }
+							if (!values.name) {
+								errors.name = "A name is required"
+							}
 
-              if (!values.message) {
-                errors.message = "A message is required";
-              }
+							if (!values.email) {
+								errors.email = "An email is required"
+							} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+								errors.email = "Invalid email address"
+							}
 
-              return errors;
-            }}
-            onSubmit={(values, { setSubmitting }) => {
-              setTimeout(() => {
-                axios
-                  .post("/api/contact", values)
-                  .then(function (response) {
-                    setFormResult({
-                      success: true,
-                      message: response.data.message,
-                    });
-                  })
-                  .catch(function (error) {
-                    setFormResult({
-                      success: false,
-                      message: error.message,
-                    });
-                  });
-                setSubmitting(false);
-              }, 400);
-            }}
-          >
-            {({ isSubmitting }) => {
-              return (
-                <Form className="md:w-1/2 w-full">
-                  <FormField title="Name" type="text" />
-                  <FormField title="Email" type="email" />
-                  <FormField title="Message" type="textarea" />
-                  <button
-                    type="submit"
-                    disabled={isSubmitting}
-                    className="block cursor-pointer border-2 bg-accent text-light p-4 mt-6 rounded shadow text-lg font-bold"
-                  >
-                    Submit
-                  </button>
-                </Form>
-              );
-            }}
-          </Formik>
-        </section>
-      </main>
-    </>
-  );
-};
+							if (!values.message) {
+								errors.message = "A message is required"
+							}
 
-export async function getStaticProps() {
-  const dataPath = path.join(process.cwd(), "data/contact.yml");
-  const data = yaml.safeLoad(fs.readFileSync(dataPath));
-  return { props: { data } };
+							return errors
+						}}
+						onSubmit={(values, { setSubmitting }) => {
+							setTimeout(() => {
+								axios
+									.post("/api/contact", values)
+									.then(function (response) {
+										setFormResult({
+											success: true,
+											message: response.data.message,
+										})
+									})
+									.catch(function (error) {
+										setFormResult({
+											success: false,
+											message: error.message,
+										})
+									})
+								setSubmitting(false)
+							}, 400)
+						}}>
+						{({ isSubmitting }) => {
+							return (
+								<Form className="md:w-1/2 mx-auto">
+									<FormField title="Name" type="text" />
+									<FormField title="Email" type="email" />
+									<FormField title="Message" type="textarea" />
+									<button
+										type="submit"
+										disabled={isSubmitting}
+										className="block cursor-pointer border-2 bg-accent text-light p-4 mt-6 mx-auto rounded shadow text-lg font-bold">
+										Submit
+									</button>
+								</Form>
+							)
+						}}
+					</Formik>
+				</section>
+			</main>
+		</>
+	)
 }
 
-export default ContactPage;
+export async function getStaticProps() {
+	const dataPath = path.join(process.cwd(), "data/contact.yml")
+	const data = yaml.safeLoad(fs.readFileSync(dataPath))
+	return { props: { data } }
+}
+
+export default ContactPage
