e214f135d46a1de51e1e9675b1aec83773711c0a
Jason Klein <kleinjason234@gmail.com>
2020-04-20 13:01:24 -0400

    Update about page

--- pages/about.js
+++ pages/about.js
@@ -1,77 +1,83 @@
-import yaml from "js-yaml";
-import fs from "fs";
-import path from "path";
+import yaml from "js-yaml"
+import fs from "fs"
+import path from "path"
 
-import Markdown from "react-markdown";
-import SEO from "../components/seo";
+import Markdown from "react-markdown"
+import SEO from "../components/seo"
+import styles from "./about.module.css"
 
-const AboutPage = ({ data }) => {
-  return (
-    <>
-      <SEO title="About" />
-      <main className="container mx-auto">
-        <section className="p-8">
-          <h2 className="font-serif font-bold md:text-5xl text-4xl heading">
-            The Mission
-          </h2>
-          <Markdown className="text-xl markdown-body">{data.mission}</Markdown>
-        </section>
-        <section className="p-8">
-          <h2 className="font-serif font-bold md:text-5xl text-4xl heading">
-            The Members
-          </h2>
-          {data.teams.map((team) => (
-            <div
-              key={team.title.replace(" ", "-").toLowerCase()}
-              className="mb-8"
-            >
-              <h3 className="font-serif text-3xl font-bold mb-2">
-                {team.title}
-              </h3>
-              <h4 className="text-lg mb-4">
-                {team.description ? (
-                  <Markdown className="markdown-body">
-                    {team.description}
-                  </Markdown>
-                ) : null}
-              </h4>
-              <div className="flex flex-wrap">
-                {team.members.map((member) => (
-                  <div
-                    className="lg:w-1/6 md:w-1/4 sm:w-1/3 w-1/2"
-                    key={
-                      member.hasOwnProperty("link")
-                        ? member.name.replace(" ", "-").toLowerCase()
-                        : member.replace(" ", "-").toLowerCase()
-                    }
-                  >
-                    {member.hasOwnProperty("link") ? (
-                      <a
-                        href={member.link}
-                        className="border-b-2 border-gray-500 inline-block mb-3"
-                      >
-                        {member.name}
-                      </a>
-                    ) : (
-                      <p className="border-b-2 border-gray-500 inline-block mb-3">
-                        {member}
-                      </p>
-                    )}
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
-        </section>
-      </main>
-    </>
-  );
-};
-
-export async function getStaticProps() {
-  const dataPath = path.join(process.cwd(), "data/about.yml");
-  const data = yaml.safeLoad(fs.readFileSync(dataPath));
-  return { props: { data } };
+const Team = ({ title, description, members }) => {
+	return (
+		<>
+			{console.log(members.length)}
+			<h2 style={{ marginTop: title === "Founder & Coordinator" ? "0" : "3rem" }} className={styles.header2}>
+				<b>{title}</b>
+			</h2>
+			<p style={{ marginBottom: "1rem" }}>
+				<Markdown>{description}</Markdown>
+			</p>
+			<section className={styles.members} style={{ ["--count"]: members.length }}>
+				{members.map(member => (
+					<a
+						href={member.link}
+						className={styles.member}
+						key={member.name.replace(" ", "-").toLowerCase()}>
+						<span>{member.name}</span>
+					</a>
+				))}
+			</section>
+		</>
+	)
 }
 
-export default AboutPage;
+const AboutPage = ({ data }) => {
+	return (
+		<>
+			<SEO title="About" />
+			<main className={styles.content}>
+				<article className={styles.article} style={{ margin: "calc(3rem + 5rem) 0 5rem 0" }}>
+					<h1
+						style={{
+							padding: "0.5rem 2rem",
+							color: "#304352",
+							textAlign: "center",
+							textTransform: "uppercase",
+							fontSize: "200%",
+							fontWeight: "bold",
+						}}>
+						Our Mission
+					</h1>
+					<section className={styles.container}>
+						<p style={{ margin: "auto 10% 5rem 10%", lineHeight: "250%", textAlign: "center" }}>
+							<Markdown
+								style={{ margin: "auto 10% 5rem 10%", lineHeight: "250%", textAlign: "center" }}>
+								{data.mission}
+							</Markdown>
+						</p>
+					</section>
+				</article>
+				<section className={styles.team}>
+					<h1 className={styles.header1}>Our Team</h1>
+					<section className={styles.container}>
+						{data.teams.map(team => (
+							<Team
+								title={team.title}
+								description={team.description}
+								members={team.members}
+								key={team.title.replace(" ", "-").toLowerCase()}
+							/>
+						))}
+					</section>
+				</section>
+			</main>
+		</>
+	)
+}
+
+export async function getStaticProps() {
+	const dataPath = path.join(process.cwd(), "data/about.yml")
+	const data = yaml.safeLoad(fs.readFileSync(dataPath))
+	return { props: { data } }
+}
+
+export default AboutPage
